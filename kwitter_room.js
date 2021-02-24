b86e77f118cb9e621006830c7e749e7993ca0fdf
tunishq <74109308+tunishq@users.noreply.github.com>
2021-02-24 17:59:08 +0000

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBuOeRmugBcG_UmYXwlZX9qF_VtZiEbwc4",
+    authDomain: "project93-96.firebaseapp.com",
+    projectId: "project93-96",
+    storageBucket: "project93-96.appspot.com",
+    messagingSenderId: "113388541146",
+    appId: "1:113388541146:web:706b9fa1216adc51e501e3",
+    measurementId: "G-YB21813RDY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
